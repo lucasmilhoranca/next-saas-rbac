517b8b6ae04f8b23273b002ac5d493eb76ee3e5e
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-05-08 19:53:19 -0300

    criando rota de excluir organização

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
+import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { seed } from './routes/seed/seed'
 
@@ -76,6 +77,7 @@ app.register(getMembership)
 app.register(getOrganization)
 app.register(getOrganizations)
 app.register(updateOrganization)
+app.register(shutdownOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running in http://localhost:3333/')
